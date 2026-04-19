f412bf9395c036ecd53e7e03a9b3172e71137ed2
RomaniaChat <34324435+RomaniaChat@users.noreply.github.com>
2026-04-19 21:07:25 +0200

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,52 +1,77 @@
 <?php
-/**
-* public
-*
-* structure adapted for kiwiirc irc client
-* see http://showchat.eu.org/blog/noutate-wp-romaniachat for more information.
-* @since  2.0
-*/
+defined('ABSPATH') or exit;
 
-defined('ABSPATH') or die("Don't change anything here!");
+function romaniachat_build_url($atts) {
 
-	function romaniachat_page( $atts ) {
-        $url = ROMANIACHAT_URLBASE.":8443/?";
-	 if (get_option('romaniachat_server') != 'https://kiwi.romaniachat.eu:8443')
-        $url = $url."".get_option('romaniachat_server');
-	 // ? is replaced whit 3 radom numbers 
-	 if (get_option('romaniachat_nick') != '')
-        $url = $url."nick=".str_replace("?", rand(100,999), get_option('romaniachat_nick'));
-     if (get_option('romaniachat_style') != '')
-        $url = $url."&theme=".get_option('romaniachat_style');
-	  $channels = isset($atts['chan']) ? $atts['chan'] : '';
-     if ($channels == '')
-        $channels = get_option('romaniachat_chan');
-     if ($channels != '')
-        $url = $url."".$channels;
+    $base = esc_url_raw(get_option('romaniachat_server'));
+    if (!$base) {
+        $base = ROMANIACHAT_URLBASE;
+    }
 
-?>
-<center><iframe marginwidth="0" marginheight="0" src="<?php echo esc_url( $url ); ?>"
-<?php
-    if (get_option('romaniachat_width') != '')
-	    echo "width=\"".esc_attr( get_option('romaniachat_width'))."\""; ?>
- <?php
-    if (get_option('romaniachat_height') != '')
-        echo "height=\"".esc_attr( get_option('romaniachat_height'))."\""; ?>
- scrolling="no" frameborder="0"></iframe></center>
-<?php
+    $params = [];
+
+    // Nick
+    $nick = get_option('romaniachat_nick');
+    if (!empty($nick)) {
+        $nick = str_replace("?", rand(100,999), $nick);
+        $params['nick'] = sanitize_text_field($nick);
+    }
+
+    // Theme
+    $style = get_option('romaniachat_style');
+    if (!empty($style)) {
+        $params['theme'] = sanitize_text_field($style);
+    }
+
+    // Build base query
+    $url = $base . '/?' . http_build_query($params);
+
+    // 👉 Channels as fragment (#)
+    $channels = isset($atts['chan']) ? $atts['chan'] : get_option('romaniachat_chan');
+
+    if (!empty($channels)) {
+        // clean spaces
+        $channels = strtolower(trim($channels));
+        $channels = str_replace(' ', '', $channels);
+
+        // to ensure that they begin with #
+        $channels = preg_replace('/(^|,)([^#])/','$1#$2',$channels);
+
+        $url .= $channels;
+    }
+
+    return $url;
 }
 
-function romaniachat( $atts ) {
-    ob_start();
-    romaniachat_page( $atts );
+function romaniachat_page($atts) {
 
+    $url = romaniachat_build_url($atts);
+
+    $width  = esc_attr(get_option('romaniachat_width'));
+    $height = esc_attr(get_option('romaniachat_height'));
+
+    ?>
+    <div style="text-align:center;">
+        <iframe 
+            src="<?php echo esc_url($url); ?>"
+            width="<?php echo $width ?: '100%'; ?>"
+            height="<?php echo $height ?: '500'; ?>"
+            frameborder="0"
+            allow="camera; microphone; fullscreen"
+            referrerpolicy="no-referrer"
+            loading="lazy">
+        </iframe>
+    </div>
+    <?php
+}
+
+function romaniachat($atts) {
+    ob_start();
+    romaniachat_page($atts);
     return ob_get_clean();
 }
-
 /**
 * shortcode
 * @since  1.0
 */
-
-add_shortcode( 'romaniachat', 'romaniachat' );
-?>
+add_shortcode('romaniachat', 'romaniachat');
